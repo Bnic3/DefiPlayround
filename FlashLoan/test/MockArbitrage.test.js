ec0ae103bd35a61d9841c8b4240dc7fa67f10964
John Nana <bnicemagnolia@gmail.com>
2022-04-16 11:35:36 +0100

    owner withdrawable test

--- FlashLoan/test/MockArbitrage.test.js
+++ FlashLoan/test/MockArbitrage.test.js
@@ -1,12 +1,28 @@
 const MockArbitrage = artifacts.require("MockArbitrage");
 const TestToken= artifacts.require("TestToken")
 
-contract("MockArbitrage",()=>{
+contract("MockArbitrage",(accounts)=>{
   it("Should own 1000 testtoken supply", async()=>{
     const mockArbitrageInstance=  await MockArbitrage.deployed();
     const testTokenInstance = await TestToken.deployed();    
-    const mockTokenBalance = (await testTokenInstance.balanceOf.call(mockArbitrageInstance.address)).toNumber();
+    const mockTokenBalance = (await testTokenInstance.balanceOf(mockArbitrageInstance.address)).toNumber();
     assert.equal(mockTokenBalance,1000)
+
+  })
+
+  it("should have all tokens withdrawable by owner", async()=>{
+    const mockArbitrageInstance =  await MockArbitrage.deployed();
+    const mockOwner = await mockArbitrageInstance.owner(); 
+    const testTokenInstance = await TestToken.deployed(); 
+    const oldTokenBalance = (await testTokenInstance.balanceOf(accounts[0])).toNumber();
+
+    await mockArbitrageInstance.withdraw(testTokenInstance.address);    
+    
+    const newTokenBalance = (await testTokenInstance.balanceOf(accounts[0])).toNumber();
+
+    assert.equal(accounts[0],mockOwner);
+    assert.equal(oldTokenBalance,0);
+    assert.equal(newTokenBalance,1000);
 
   })
 })
